a8339e7879faafb09cefc2db158d29314a112334
malcolm <canterburymalcolm@gmail.com>
2019-04-19 00:29:40 -0400

    Prepared for heroku deployment

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('port', process.env.PORT || 3001);
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('In production, serving static assets');
   app.use(express.static('client/build'));
 }
 
