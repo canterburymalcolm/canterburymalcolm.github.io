453e715c1146fb8fe1a0e08f5cfea8804610a3cc
malcolm <canterburymalcolm@gmail.com>
2019-04-19 07:14:44 -0400

    removed production build from git ignore

---
+++ client/build/precache-manifest.8f8ebeba18ab79db882fb6e1f7e5dfe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8aeea4255f2a1f495059a37bd3284a",
+    "url": "/static/media/user-info-bubble.df8aeea4.png"
+  },
+  {
+    "revision": "86c3f857e2876b8b9a177c6c74bdf94d",
+    "url": "/static/media/parent-bubble.86c3f857.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c3ca02c85d1e3947d2bc",
+    "url": "/static/js/main.548804bd.chunk.js"
+  },
+  {
+    "revision": "8ea0ed38e270d206f3e6",
+    "url": "/static/js/2.e68ccd1f.chunk.js"
+  },
+  {
+    "revision": "c3ca02c85d1e3947d2bc",
+    "url": "/static/css/main.d876d8a5.chunk.css"
+  },
+  {
+    "revision": "59e4847a8441ceb9379741dd16949488",
+    "url": "/index.html"
+  }
+];
